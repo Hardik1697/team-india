8301deacd1e29fc87d2483f3d925273c409c402c
Ishu <psingh@email.sc.edu>
2020-04-05 14:16:50 -0400

    Pushing to master

---
+++ Movie.java
@@ -0,0 +1,38 @@
+
+public class Movie
+{
+	private String movieName;
+	private int movieID;
+	private boolean availability;
+
+	public String getMovieName()
+	{
+		return movieName;
+	}
+
+	public void setMovieName(String movieName)
+	{
+		this.movieName = movieName;
+	}
+
+	public int getMovieID()
+	{
+		return movieID;
+	}
+
+	public void setMovieID(int movieID)
+	{
+		this.movieID = movieID;
+	}
+
+	public boolean isAvailability()
+	{
+		return availability;
+	}
+
+	public void setAvailability(boolean availability)
+	{
+		this.availability = availability;
+	}
+
+}
---
+++ Theater.java
@@ -0,0 +1,7 @@
+public class Theater
+{
+	public String theatreName;
+	public int theatreID;
+	public String address;
+
+}
